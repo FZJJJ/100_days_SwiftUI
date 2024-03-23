235e496de502fbb058d603dd9de5107e4636ba14
FZJ <2919839271@qq.com>
2024-03-23 21:07:20 +0800

    checkpoint3

---
+++ checkPoint3.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Cocoa
+
+for num in 1...100 {
+    if num.isMultiple(of: 3) && num.isMultiple(of: 5) {
+        print("FizzBuzz")
+    }
+    else if num.isMultiple(of: 3) {
+        print("Fizz")
+    }
+    else if num.isMultiple(of: 5) {
+        print("Buzz")
+    }
+    else {
+        print(num)
+    }
+}
