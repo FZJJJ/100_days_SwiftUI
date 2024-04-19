b8f2101e0483e5153c3de5bd27aa4ae360f0f8c6
FZJ <2919839271@qq.com>
2024-04-19 12:02:26 +0800

    Day28-Challenge

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by FZJ on 2024/4/17.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
