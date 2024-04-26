ad477e29e70de06675c7c4bd7a7e1915b93f738f
FZJ <2919839271@qq.com>
2024-04-26 12:42:37 +0800

    project7

---
+++ iExpenseApp/iExpenseApp/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpenseApp
+//
+//  Created by FZJ on 2024/4/25.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
---
+++ iExpenseApp/iExpenseApp/iExpenseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseAppApp.swift
+//  iExpenseApp
+//
+//  Created by FZJ on 2024/4/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
