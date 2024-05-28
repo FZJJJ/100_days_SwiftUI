83081492b7d76d31320c70e43a7d4e6bcd6b0e56
FZJ <2919839271@qq.com>
2024-05-28 11:57:05 +0800

    InstaFilter

---
+++ InstaFilter/InstaFilter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InstaFilter
+//
+//  Created by FZJ on 2024/5/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InstaFilter/InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by FZJ on 2024/5/27.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
